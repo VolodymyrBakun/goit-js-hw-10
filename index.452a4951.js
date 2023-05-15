09eeadde0ff7c16edc6e94202b2956229c2686c3
VolodymyrBakun <119749890+VolodymyrBakun@users.noreply.github.com>
2023-05-15 20:09:01 +0000

    Deploying to gh-pages from @ VolodymyrBakun/goit-js-hw-10@a579e14f987a84f0f6b4bb74126f4baca9fcc81f 🚀

--- index.452a4951.js
+++
@@ -1,2 +0,0 @@
-var e=refs={inputEl:document.querySelector("#search-box")};e.inputEl.addEventListener("input",(function(){const t=e.inputEl.value.trim();if(""===t)return alert("Потрібно щось ввести!");fetch(`https://restcountries.com/v3.1/name/${t}?fields=name,capital,population,flag,languages`).then((e=>console.log(e.json())))}));
-//# sourceMappingURL=index.452a4951.js.map
